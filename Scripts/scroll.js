81c7e168dc46f9b10042331e33f20ba3e138100b
RoshanKumar770 <roshankumar81624@gmail.com>
2024-09-15 23:58:00 +0530

    finalize

--- Scripts/scroll.js
+++ Scripts/scroll.js
@@ -1,43 +1,47 @@
-var arr = [
-  {
-  item :"https://img2.rtve.es/i/?w=1600&i=1635840595818.jpg"
-  , info:"https://files.thehandbook.com/uploads/2021/02/aauvwnhpd-iljx32sgpmy-zvogfwtaub4ydo3f3jnt1csgs800-c-k-c0x00ffffff-no-rj-mo.jpg"
- },
-  {
-  item :"https://staticg.sportskeeda.com/editor/2023/06/6e3bd-16875365347315-1920.jpg"
-  , info:"https://www.euphoriazine.com/wp-content/uploads/2023/08/maluma.jpg"
- },
-  {
-  item :"https://fangirlish.com/wp-content/uploads/2022/01/Through-My-Window-2.jpg"
-  , info:"https://img2.rtve.es/i/?w=1600&i=1635840595818.jpg"
- },
-  {
-  item :"https://files.thehandbook.com/uploads/2021/02/aauvwnhpd-iljx32sgpmy-zvogfwtaub4ydo3f3jnt1csgs800-c-k-c0x00ffffff-no-rj-mo.jpg"
-  , info:"https://staticg.sportskeeda.com/editor/2023/06/6e3bd-16875365347315-1920.jpg"
- },
-  {
-  item :"https://img2.rtve.es/i/?w=1600&i=1635840595818.jpg"
-  , info:"https://staticg.sportskeeda.com/editor/2023/06/6e3bd-16875365347315-1920.jpg"
- },
-]
+// var arr = [
+//   {
+//   item :"https://img2.rtve.es/i/?w=1600&i=1635840595818.jpg"
+//   , info:"https://files.thehandbook.com/uploads/2021/02/aauvwnhpd-iljx32sgpmy-zvogfwtaub4ydo3f3jnt1csgs800-c-k-c0x00ffffff-no-rj-mo.jpg"
+//  },
+//   {
+//   item :"https://staticg.sportskeeda.com/editor/2023/06/6e3bd-16875365347315-1920.jpg"
+//   , info:"https://www.euphoriazine.com/wp-content/uploads/2023/08/maluma.jpg"
+//  },
+//   {
+//   item :"https://fangirlish.com/wp-content/uploads/2022/01/Through-My-Window-2.jpg"
+//   , info:"https://img2.rtve.es/i/?w=1600&i=1635840595818.jpg"
+//  },
+//   {
+//   item :"https://files.thehandbook.com/uploads/2021/02/aauvwnhpd-iljx32sgpmy-zvogfwtaub4ydo3f3jnt1csgs800-c-k-c0x00ffffff-no-rj-mo.jpg"
+//   , info:"https://staticg.sportskeeda.com/editor/2023/06/6e3bd-16875365347315-1920.jpg"
+//  },
+//   {
+//   item :"https://img2.rtve.es/i/?w=1600&i=1635840595818.jpg"
+//   , info:"https://staticg.sportskeeda.com/editor/2023/06/6e3bd-16875365347315-1920.jpg"
+//  },
+// ]
 
-var box = document.querySelector("#box");
-var clutter = " ";
-arr.forEach(function(elem,idx){
-clutter +=`
-<div class="box">
-    <img id="${idx}" src="${elem.item}">
-  </div>`;
-});
-box.innerHTML = clutter;
-box.addEventListener("click", function (dets) {
+// var box = document.querySelector("#box");
+// var clutter = " ";
+// arr.forEach(function(elem,idx){
+// clutter +=`
+// <div class="box">
+//     <img id="${idx}" src="${elem.item}">
+//   </div>`;
+// });
+// box.innerHTML = clutter;
+// box.addEventListener("click", function (dets) {
 
-  // console.log(arr[dets.target.id].story); golden key
-  document.querySelector("#full-screen").style.display = "block";
-  document.querySelector("#full-screen").style
-    .backgroundImage = `url(${arr[dets.target.id].info})`
-});
+//   // console.log(arr[dets.target.id].story); golden key
+//   document.querySelector("#full-screen").style.display = "block";
+//   document.querySelector("#full-screen").style
+//     .backgroundImage = `url(${arr[dets.target.id].info})`
+// });
 
-setTimeout(function () {
-  document.querySelector("#full-screen").style.display = "none";
-}, 3000);
+// setTimeout(function () {
+//   document.querySelector("#full-screen").style.display = "none";
+// }, 3000);
+var img = document.querySelector("");
+img.addEventListener("dblclick",function(img){
+
+})
